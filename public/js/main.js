9e50e70f5b25066be7351401442914e6f44387a6
ultrayoshi <david@imesmes.com>
2013-12-15 15:28:20 +0100

    Add requirejs plugins as a dependency

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,19 @@
 requirejs.config({
+    basePath: 'js',
     paths: {
-        jquery: '../bower_components/jquery/jquery'
+        jquery: '../bower_components/jquery/jquery',
+        data: '../data',
+        json: '../bower_components/requirejs-plugins/src/json',
+        text: '../bower_components/requirejs-plugins/lib/text'
     }
 });
 
 define(function (require) {
-    var $ = require('jquery');
+    var $       = require('jquery'),
+        level0  = require('json!data/level0.json'); // usando require.js podemos leer ficheros json ahora
     
     $(function () {
-        console.log("DOM ready");
+        console.log('DOM ready');
+        console.log(level0);
     });
 });
