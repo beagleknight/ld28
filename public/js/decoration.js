d9cd4fccf8753d23ca54758b14254a2fbe785216
ultrayoshi <david@imesmes.com>
2013-12-15 21:44:46 +0100

    Add Decoration entity

---
+++ public/js/decoration.js
@@ -0,0 +1,13 @@
+define(function(require) {
+    var Entity = require('entity');
+    
+    var Decoration = function(name, position) {
+        Entity.call(this, name, position);
+        this.broken = false;
+    };
+    Decoration.prototype = new Entity(null, {});
+    
+    Decoration.prototype.isBroken = function () {
+        return this.broken;
+    };
+});
