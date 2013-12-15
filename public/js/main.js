67681bfdd3833696dc559c83eecd93bc6cf85201
ultrayoshi <david@imesmes.com>
2013-12-15 15:17:15 +0100

    Add jquery as a dependency

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,13 @@
-define(function () {
-    console.log("TODO main");
+requirejs.config({
+    paths: {
+        jquery: '../bower_components/jquery/jquery'
+    }
+});
+
+define(function (require) {
+    var $ = require('jquery');
+    
+    $(function () {
+        console.log("DOM ready");
+    });
 });
