54b157a3f54c5c09f8968687cb63d3c9b058c5cd
rcastropulido <rcastropulido@gmail.com>
2013-12-15 12:31:03 +0100

    Definición del método render.

--- js/entity.js
+++ js/entity.js
@@ -9,9 +9,21 @@
         this.posX = posX;
         this.posY = posY;
     };  
-    Entity.prototype.render = function () {
-        console.log("Estoy dibujandoooooooooo.");
+    Entity.prototype.render = function (ctx) {
+        var posX = this.posX || 0, posY = this.posY || 0;
+        
+console.log('Estoy dibujandoooooooooo.');
+        ctx.beginPath();
+
+        ctx.moveTo(posX - 5, posY);
+        ctx.lineTo(posX + 5, posY);
+        ctx.moveTo(posX, posY - 5);
+        ctx.lineTo(posX, posY + 5);
+        
+        ctx.strokeStyle = 'blue';
+        ctx.stroke();
     };
+    
 
     return Entity;
 });
